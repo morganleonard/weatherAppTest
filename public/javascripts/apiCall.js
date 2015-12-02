c0264ab609cab58ce20644cb0b19bd486a4b7f72
Morgan Leonard <morganleonardpublic@gmail.com>
2015-12-02 02:29:26 -0800

    api call works for autoip, builds data object with forecast

--- public/javascripts/apiCall.js
+++ public/javascripts/apiCall.js
@@ -2,4 +2,52 @@ $(document).ready( function () {
 
 	console.log("reading apiCall.js")
 
+	// get location
+	var location = null;
+
+	//build url
+	var url = null;
+
+	$.ajax({
+		url : "http://api.wunderground.com/api/8c1a8ef70cc596b2/forecast/q/autoip.json",
+  		dataType : "jsonp",
+  		success : function(parsed_json) {
+  			console.log("parsed_json");
+  			console.log(parsed_json)
+
+  			var data = [];
+
+  			for(var i = 0; i < 4; i++) {
+
+  				var dayData = {
+  					temp    : null,
+  					iconUrl : null,
+  					weekday : null,
+  				};
+
+	  			dayData.temp = parsed_json.forecast.simpleforecast.forecastday[i].high.fahrenheit;
+	  			// console.log("temp");
+	  			// console.log(temp);
+
+	  			dayData.iconUrl = parsed_json.forecast.simpleforecast.forecastday[i].icon_url;
+	  			// console.log("iconUrl");
+	  			// console.log(iconUrl);
+	  			
+	  			dayData.weekday = parsed_json.forecast.simpleforecast.forecastday[i].date.weekday;
+	  			// console.log("weekday");
+	  			// console.log(weekday);
+
+	  			data.push(dayData);
+
+	  		}
+
+	  		console.log("data");
+	  		console.log(data);
+
+  			//var location = parsed_json['location']['city'];
+  			//var temp_f = parsed_json['current_observation']['temp_f'];
+  			//alert("Current temperature in " + location + " is: " + temp_f);
+  		}
+  	});
+
 })
